4eb6e4bc2f67ffb728bfbbe6339fe3232b78a913
Iqra Arif <bintearif79@gmail.com>
2025-05-17 23:31:06 -0700

    Add files via upload

---
+++ String assignment.py
@@ -0,0 +1,42 @@
+#Task 1:Characters
+# Characters is a single letter ,digit or symble.It is written in sigle quotes
+#Example:
+char1 = 'A'
+char2 = '7'
+char3 = '#'
+print (char1,char2,char3)
+#Task 2: String
+#The texts words or  Characters is a string.Anything write in double quotes.
+favorite_animal = "Butterfly"
+senence_about_animal = "Butterflies have soft wings with lovely patterns."
+print(favorite_animal)
+print(senence_about_animal)
+#Task 3:Concatenation
+#Concatenation is a joining string together.
+first_name = "Iqra"
+last_name = "Arif"
+full_name = first_name +" "+ last_name
+print = (full_name) 
+#Task 3:Repetition
+#Repetition mean repeating a string multiply times using the * sign.
+laugh = "Hi"
+print(laugh * 5)
+#Task 4:Indexing
+#Indexing mean getting a specifice a string using its position.It start from zero.
+Messages = "PythonRocks"
+#Printing the first characters
+print(Messages[0])
+#Printing the fourth characters
+print(Messages[3])
+#Printing the last characters
+print(Messages[-1])
+#Task 5:Slicing
+#Slicing mean getting a part a string using a rang of position.
+Ashir = "I love coding in Python"
+#Slicing the word "love"(start at the index 2,end before index 6.)
+print(Ashir[2:6])
+#Slicing the word "Python" from the end.
+print(Ashir[-6:])
+#Bonus Task 
+name = "Iqra"
+print("Welcome," + name +"! " * 3)
